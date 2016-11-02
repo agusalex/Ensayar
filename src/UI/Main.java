8d48a6cd8c68ee6c17cfd4bf8a7b816ed9d45033
Agus <agusalexander@hotmail.com>
2016-11-02 00:54:14 -0300

    JavaFx

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -11,9 +11,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Ensayos");
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
+        primaryStage.setResizable(false);
+
+
     }
 
 
